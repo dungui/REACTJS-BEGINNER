31a5fb78f03bc9ec3c67b4c43d2dc47edaf88251
macbook-pro <macbook-pro@macbook-pros-MacBook-Pro.local>
2024-08-28 05:12:10 +0300

    First repoon react

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+
+
+function Nav(){
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Address</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
