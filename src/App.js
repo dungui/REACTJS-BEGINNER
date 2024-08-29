63b5edc00787434fd8ab2c67c86105c0bc195c1f
macbook-pro <macbook-pro@macbook-pros-MacBook-Pro.local>
2024-08-29 08:36:33 +0300

    changes effected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="second card's h2" h3="second card's h3"/>
+      <Card h2="third card's h2" h3="third card's h3"/>
     </div>
   );
 }
